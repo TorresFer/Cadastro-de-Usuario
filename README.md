026b4b08915595fe13463080508b9bc929ce19fd
Fernando Torres <fernandorox1@hotmail.com>
2022-05-17 12:08:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Cadastro de Usuário
+Em andamento
+
+## Tecnologias
+Frontend - React
+Backend - JSON-Server; API Rest
+
+## Links
+
+___________________
+
+Feito por Torres Fer
---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i> por
+            <strong>Torr <span className='text-danger'>E</span>S</strong>
+        </span>
+    </footer>
